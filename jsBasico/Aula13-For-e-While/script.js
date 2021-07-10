91cfa9a5d1bf58262132688a5f68de479c968700
Sergio Baumgartner <brunohbaumgartner@hotmail.com>
2021-07-10 13:03:42 -0300

    Commit dia 10/07

---
+++ jsBasico/Aula13-For-e-While/script.js
@@ -0,0 +1,27 @@
+/*
+Laço para contar numeros com for
+*/
+
+var numero = 1000;
+
+for (let vez = 0; vez<=numero; vez+=5) {
+    console.log(vez);
+    
+}
+
+
+
+
+/*
+Laço para contar com while
+*/
+
+
+/* while (numero < 20) {
+
+    console.log(numero);
+    numero += 5;
+    
+} */
+
+console.log("Acabou");
---
+++ jsBasico/Aula14-arrays/script.js
@@ -0,0 +1,14 @@
+/* var alunos = new Array("Sergio", "Bruno", "Ana", "Vitoria");
+
+console.log(alunos[2]); */
+
+
+var alunos = ["Sergio", "Bruno", "Ana", "Vitoria"];
+
+
+console.log(alunos[2]);
+
+
+for(var i in alunos){
+    console.log(alunos[i])
+}
